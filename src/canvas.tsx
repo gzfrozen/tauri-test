23aafe40c2cb68f2f8ee53923eb341fde1598514
gzfrozen <gzliuxiaochen@gmail.com>
2022-09-30 22:15:51 +0900

    add resolution parameter.

--- src/canvas.tsx
+++ src/canvas.tsx
@@ -1,14 +1,27 @@
 import { invoke } from "@tauri-apps/api";
-import { Component, createSignal, For, onCleanup, onMount } from "solid-js";
+import {
+  Accessor,
+  Component,
+  createSignal,
+  For,
+  onCleanup,
+  onMount,
+} from "solid-js";
+import "./canvas.css";
+
+const resolution = { x: 80, y: 40 };
 
 const Canvas: Component = () => {
   const [pixel, setPixel] = createSignal(true);
   const [pixelSize, setPixelSize] = createSignal("0");
+  const pixels = Array<Accessor<boolean>>(resolution.x * resolution.y).fill(
+    pixel
+  );
 
   const getPixelSize = () => {
     const canvasElement = canvas as HTMLDivElement;
     console.log(canvasElement.clientWidth);
-    setPixelSize((canvasElement.clientWidth / 3).toString() + "px");
+    setPixelSize((canvasElement.clientWidth / resolution.x).toString() + "px");
   };
 
   window.addEventListener("resize", getPixelSize);
@@ -27,34 +40,20 @@ const Canvas: Component = () => {
       .catch((e) => console.error(e));
   };
 
-  const pixels = [
-    pixel,
-    pixel,
-    pixel,
-    pixel,
-    pixel,
-    pixel,
-    pixel,
-    pixel,
-    pixel,
-  ];
-
   const canvas = (
     <div
       class="grid m-10"
       style={{
-        "grid-template-columns": "repeat(3, 1fr)",
-        "grid-template-rows": "repeat(3, 1fr)",
+        "grid-template-columns": `repeat(${resolution.x}, ${pixelSize()})`,
+        "grid-template-rows": `repeat(${resolution.y}, ${pixelSize()})`,
       }}
     >
       <For each={pixels}>
         {(pixel) => (
           <div
-            class="border-2 border-red-600"
+            class="pixel border-red-600"
             style={{
               "background-color": pixel() ? "black" : "white",
-              height: pixelSize(),
-              width: pixelSize(),
             }}
             onclick={render}
           ></div>
